fb16c863d40ce378ecf6ad400ddf4a5fccc4622e
aathwal3 <aathwal3@gmail.com>
2015-11-07 15:00:09 -0800

    add secret keys and helper logic for twilio initial and final text messages- still need logic for contact

--- Gemfile
+++ Gemfile
@@ -30,7 +30,8 @@ gem 'bcrypt', '~> 3.1.7'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
-
+gem 'twilio-ruby'
+gem 'envyable'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,28 @@
+require 'rubygems' 
+require 'twilio-ruby' 
+
+account_sid = ENV['TWILIO_ACCOUNT_SID']
+auth_token = ENV['TWILIO_AUTH_TOKEN']
+from_number = ENV['TWILIO_FROM_NUMBER']
+@client = Twilio::REST::Client.new account_sid, auth_token
+
 module UsersHelper
+	def send_initial_text
+		@client.account.messages.create({
+		  :from => from_number,
+		  :to => customer_number #needs to be defined from db,
+		  :body => '#{current_user.username} is using SafeWalk and wants to notify you that they are on their way home.' 
+		})
+	end
+
+	def send_destination_text
+		if #js logic for reaching home
+					@client.account.messages.create({
+		  :from => from_number,
+		  :to => customer_number,#needs to be defined from db
+		  :body => '#{current_user.username} has arrived home safely.' 
+		})
+		end
+	end
+
 end
