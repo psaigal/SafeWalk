f8b7a1f715fae92663509e5fc60ea4c5f7e44a65
Dean Solecki <deansolecki@gmail.com>
2015-11-09 17:46:36 -0800

    cleaned up gemfile and added views/routes for testing auth

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby "2.0.0"
+ruby "2.2.1"
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
@@ -25,8 +25,6 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
-
-gem 'rails_12factor', group: :production
 gem 'puma'
 
 # Use Unicorn as the app server
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,33 +18,33 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  def send_initial_text
-  	account_sid = ENV['TWILIO_ACCOUNT_SID']
-		auth_token = ENV['TWILIO_AUTH_TOKEN']
-		from_number = ENV['TWILIO_FROM_NUMBER']
-		@client = Twilio::REST::Client.new account_sid, auth_token
-		@client.account.messages.create({
-		  :from => from_number,
-		  :to => User.first.contacts.first.phone_number,
-		  :body => 'is using SafeWalk and wants to notify you that they are on their way home.'
-		})
-	end
-
-
-  def send_destination_text
-    if arrived
-      account_sid = ENV['TWILIO_ACCOUNT_SID']
-      auth_token = ENV['TWILIO_AUTH_TOKEN']
-      from_number = ENV['TWILIO_FROM_NUMBER']
-      @client = Twilio::REST::Client.new account_sid, auth_token
-  	  #connect with dean on routes for true from js
-  		@client.account.messages.create({
-  	  :from => from_number,
-  	  :to => User.first.contacts.first.phone_number,
-  	  :body => ' has arrived home safely.'
-  	})
-      return "You have notified your loved ones"
-    end
-	end
+#  def send_initial_text
+#  	account_sid = ENV['TWILIO_ACCOUNT_SID']
+#		auth_token = ENV['TWILIO_AUTH_TOKEN']
+#		from_number = ENV['TWILIO_FROM_NUMBER']
+#		@client = Twilio::REST::Client.new account_sid, auth_token
+#		@client.account.messages.create({
+#		  :from => from_number,
+#		  :to => User.first.contacts.first.phone_number,
+#		  :body => 'is using SafeWalk and wants to notify you that they are on their way home.'
+#		})
+#	end
+#
+#
+#  def send_destination_text
+#    if arrived
+#      account_sid = ENV['TWILIO_ACCOUNT_SID']
+#      auth_token = ENV['TWILIO_AUTH_TOKEN']
+#      from_number = ENV['TWILIO_FROM_NUMBER']
+#      @client = Twilio::REST::Client.new account_sid, auth_token
+#  	  #connect with dean on routes for true from js
+#  		@client.account.messages.create({
+#  	  :from => from_number,
+#  	  :to => User.first.contacts.first.phone_number,
+#  	  :body => ' has arrived home safely.'
+#  	})
+#      return "You have notified your loved ones"
+#    end
+#	end
 
 end
